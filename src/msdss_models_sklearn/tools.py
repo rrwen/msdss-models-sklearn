77e724dd7f4e41be47a4268237ba1ebc30d96abc
Richard Wen <rrwen.dev@gmail.com>
2021-12-24 22:18:35 -0500

    Move models to core

--- src/msdss_models_sklearn/tools.py
+++ src/msdss_models_sklearn/tools.py
@@ -1,12 +1,4 @@
-import inspect
 import pandas
-import sklearn
-import sys
-
-from msdss_models_api.models import Model
-from sklearn import *
-
-from .defaults import *
 
 def create_input_method(model):
     def input(self, data, x=None, y=None, _fit={}, *args, **kwargs):
@@ -64,27 +56,3 @@ def create_update_method():
         # (create_update_method_update) Update model instance
         self.instance.fit(data_x, data_y, *args, **kwargs)
     return update
-    
-def get_sklearn_models(modules=DEFAULT_MODULES):
-    out = {}
-    for name in modules:
-
-        # (get_sklearn_models_module) Get the sklearn module
-        module = getattr(sklearn, name)
-
-        # (get_sklearn_models_filter1) Filter for first letter caps and no underscore in module namespace
-        module_models = [getattr(module, m) for m in dir(module) if m[0] == m[0].upper() and m[0] != '_']
-
-        # (get_sklearn_models_filter2) Filter for classes that are inside the module only
-        module_models = [m for m in module_models if inspect.isclass(m) and m.__module__.startswith(f'sklearn.{name}')]
-
-        # (get_sklearn_models_create) Create msdss model and add to dict
-        for model in module_models:
-            model_name = f'sklearn.{name}.{model.__name__}'
-            out[model_name] = type(model.__name__, (Model,), {
-                'input': create_input_method(model),
-                'output': create_output_method(),
-                'update': create_update_method()
-            })
-            setattr(sys.modules[__name__], model.__name__, out[model_name])
-    return out
